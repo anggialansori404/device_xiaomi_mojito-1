1e8992acc29af3eb8b8ee5a42fd140772b2b1810
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-08-27 14:55:44 +0000

    mojito: update fp to August
    Signed-off-by: anggialansori404 <anggialansori.rpl4@gmail.com>

--- nad_mojito.mk
+++ nad_mojito.mk
@@ -30,9 +30,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
   
-BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mojito \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210705.001 7380771 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
     
